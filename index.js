52cd5b8e3b8aaae805c02ac34e288efb98c5a294
James Turle <james@foyerlive.com>
2018-02-09 17:30:33 -0500

    try this

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
 "use strict";
 
-if (window.chrome && window.chrome.storage && window.chrome.storage.local)
-  window.localStorage = chrome.storage.local;
 module.exports = require('./lib/soap');
-console.debug('localStorage overridden');
+// console.debug('localStorage overridden');
