ad55cf88618fc27958187069c2dedea404f3a899
James Turle <james@foyerlive.com>
2018-02-09 15:16:33 -0500

    lazy

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 "use strict";
 
-if (chrome && chrome.storage && chrome.storage.local)
+if (window.chrome && window.chrome.storage && window.chrome.storage.local)
   window.localStorage = chrome.storage.local;
 module.exports = require('./lib/soap');
 console.debug('localStorage overridden');
